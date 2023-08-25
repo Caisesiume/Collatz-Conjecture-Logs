1484d1a4af89b8e0ccd7cdbfaa648c531f321d15
Caisesiume <simonarvidsson91@gmail.com>
2023-08-25 03:44:35 +0200

    added additional stats insights

--- 3NPlus1.c
+++ 3NPlus1.c
@@ -1,32 +1,68 @@
+// 3n + 1 algorithm by @Caisesiume
 #include <stdio.h>
 
-int collatzSequence(int n) {
-    int length = 1;
+typedef struct {
+    int length;
+    int highestValue;
+} SequenceInfo;
+
+SequenceInfo collatzSequence(int n) {
+    SequenceInfo info;
+    info.length = 1;
+    info.highestValue = n;
+    printf("%d: ", n);
+
     while (n != 1) {
+        printf("%d, ", n);
         if (n % 2 == 0) {
             n /= 2;
         } else {
             n = 3 * n + 1;
         }
-        length++;
+        if (n > info.highestValue) {
+            info.highestValue = n;
+        }
+        info.length++;
     }
-    return length;
+    printf("1\n");
+    return info;
 }
 
 int main() {
-    int upperLimit = 50; // How high/long the algorithm will test
-    int longest = 0; // Length of longest sequence
-    int longestNumber = 0; // Number that had the longest sequence
+    int upperLimit = 50;
+    int highestNumber = 0;
+    int highestNumberIteration = 0;
+    int overallHighest = 0;
+    int longest = 0;
+    int longestIteration = 0;
+    int totalVisited = 0;
+    int totalSequences = 0;
 
     for (int i = 1; i <= upperLimit; i++) {
-        int sequenceLength = collatzSequence(i);
-        if (sequenceLength > longest) {
-            longest = sequenceLength;
-            longestNumber = i;
+        SequenceInfo sequenceInfo = collatzSequence(i);
+        totalVisited += sequenceInfo.length;
+        totalSequences++;
+
+        if (i > highestNumber) {
+            highestNumber = i;
+        }
+
+        if (sequenceInfo.length > longest) {
+            longest = sequenceInfo.length;
+            longestIteration = i;
+        }
+
+        if (sequenceInfo.highestValue > overallHighest) {
+            overallHighest = sequenceInfo.highestValue;
+            highestNumberIteration = i;
         }
     }
 
-    printf("Number %d had the longest sequence of %d nodes.\n", longestNumber, longest);
+    printf("\n-----STATS AFTER CALCULATIONS-----\n");
+    printf("Overall highest integer reached: %d, reached in iteration %d\n", overallHighest, highestNumberIteration);
+    printf("Longest sequence: %d nodes, occurred in iteration: %d\n", longest - 1, longestIteration);
+    printf("Total numbers (nodes) visited: %d\n", totalVisited);
+    printf("Amount of numbers (sequences) calculated: %d\n", totalSequences);
 
     return 0;
 }
